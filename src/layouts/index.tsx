f118d93e73775c5dabd7f570e0168fd743aa1bdb
way-zer <himc.wicp@gmail.com>
2020-08-03 13:04:53 +0800

    修复手机端导航菜单问题

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,26 +1,43 @@
 import { IRouteComponentProps, Link } from 'umi';
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Button, Dropdown, Grid, Layout, Menu } from 'antd';
+import { UnorderedListOutlined } from '@ant-design/icons';
+
 export default function({ children, location }: IRouteComponentProps) {
+  const screens = Grid.useBreakpoint();
+  const menu = (
+    <Menu
+      theme={screens.xs ? 'light' : 'dark'}
+      mode={screens.xs ? undefined : 'horizontal'}
+      selectedKeys={[location.pathname.split('/')[1]]}
+      style={{ display: 'inline-block', verticalAlign: 'top' }}
+    >
+      <Menu.Item key={'maps'}>
+        <Link to={'/maps'}>地图</Link>
+      </Menu.Item>
+      <Menu.Item key={'servers'}>
+        <Link to={'/servers'}>服务器列表</Link>
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <Layout>
       <Layout.Header>
         <span style={{ color: 'goldenrod', fontSize: '22px' }}>
           Mindustry 资源站{' '}
         </span>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={[location.pathname.split('/')[0]]}
-          style={{ display: 'inline-block', verticalAlign: 'top' }}
-        >
-          <Menu.Item key={'maps'}>
-            <Link to={'/maps'}>地图</Link>
-          </Menu.Item>
-          <Menu.Item key={'servers'}>
-            <Link to={'/servers'}>服务器列表</Link>
-          </Menu.Item>
-        </Menu>
+        {screens.xs ? (
+          <Dropdown overlay={menu}>
+            <Button
+              ghost
+              style={{ position: 'absolute', top: '16px', right: '24px' }}
+            >
+              <UnorderedListOutlined />
+            </Button>
+          </Dropdown>
+        ) : (
+          menu
+        )}
       </Layout.Header>
       <Layout.Content>{children}</Layout.Content>
       <Layout.Footer style={{ textAlign: 'center' }}>

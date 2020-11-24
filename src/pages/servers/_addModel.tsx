ac0fcc2989f5f1e4e55d7e8892075266c9d06d85
way-zer <himc.wicp@gmail.com>
2020-11-24 20:37:27 +0800

    [Update](/servers/add) 使用函数重写React组件

--- src/pages/servers/_addModel.tsx
+++ src/pages/servers/_addModel.tsx
@@ -1,59 +1,30 @@
-import React from 'react';
-import { Alert, Input, Modal, Spin } from 'antd';
+import React, { useState } from 'react';
+import { Input, Modal, Spin } from 'antd';
 import { request } from '@@/plugin-request/request';
 
-export class AddModel extends React.Component<
-  { close: (update: boolean) => void },
-  { error?: string; loading: boolean }
-> {
-  state = { error: undefined, loading: false };
-  inputR = React.createRef<Input>();
-
-  render() {
-    return (
-      <>
-        <Modal
-          title="请输入服务器地址"
-          visible
-          onCancel={() => {
-            this.props.close(false);
-          }}
-          onOk={this.check.bind(this)}
-        >
-          {this.state.error && (
-            <Alert message={this.state.error} type={'error'} />
-          )}
-          <Spin spinning={this.state.loading}>
-            <Input
-              type={'text'}
-              ref={this.inputR}
-              onSubmit={this.check.bind(this)}
-            />
-          </Spin>
-        </Modal>
-      </>
-    );
+export function AddModel({ close }: { close: (update: boolean) => void }) {
+  const [loading, setLoading] = useState(false);
+  const inputR = React.createRef<Input>();
+  async function check() {
+    const value = inputR.current!!.state.value;
+    setLoading(true);
+    await request('/api/servers/add?address=' + value).finally(() => {
+      setLoading(false);
+    });
+    close(true);
   }
-
-  check() {
-    const value = this.inputR.current!!.state.value;
-    this.setState({ loading: true });
-    request('/api/servers/add?address=' + value, { skipErrorHandler: true })
-      .then(() => {
-        this.props.close(true);
-      })
-      .catch(async e => {
-        if (e.response.status == 406)
-          this.setState({ error: '连接服务器失败，请检查地址是否正确！' });
-        else if (e.response.status == 403)
-          this.setState({ error: '错误:' + (await e.response.text()) });
-        else {
-          this.setState({ error: '未知错误' + e });
-        }
-        console.error(e);
-      })
-      .finally(() => {
-        this.setState({ loading: false });
-      });
-  }
+  return (
+    <Modal
+      title="请输入服务器地址"
+      visible
+      onCancel={() => {
+        close(false);
+      }}
+      onOk={check}
+    >
+      <Spin spinning={loading}>
+        <Input type={'text'} ref={inputR} onSubmit={check} />
+      </Spin>
+    </Modal>
+  );
 }

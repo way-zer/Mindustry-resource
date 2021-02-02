20e866f12cfcffa4e6c3308b401603e5cfc3443a
way-zer <himc.wicp@gmail.com>
2021-02-02 19:08:06 +0800

    [Update](maps/download) 更新加载接口

--- src/pages/maps/_components/ActionDownload.tsx
+++ src/pages/maps/_components/ActionDownload.tsx
@@ -3,20 +3,14 @@ import React, { ReactElement } from 'react';
 import { requestToken } from '@/utils/reCaptcha';
 import { useModel } from '@@/plugin-model/useModel';
 
-export function ActionDownload({
-  hash,
-  content,
-}: {
-  hash: string;
-  content: (onClick: () => void) => ReactElement;
-}) {
+export function ActionDownload({ hash, content }: { hash: string; content: (onClick: () => void) => ReactElement }) {
   const { info } = useModel('user');
   return (
     <Tooltip title={'下载地图'} key={'download'}>
       {content(async () => {
         if (info == null) return message.error('请先登录后再下载', 10);
         const token = await requestToken('mapDownload');
-        window.open(`/api/maps/download?token=${token}&id=${hash}`);
+        window.open(`/api/maps/${hash}/download?token=${token}`);
       })}
     </Tooltip>
   );

593f71bb6312c4eebbf7503bfc2d97c0fd748bf9
way-zer <himc.wicp@gmail.com>
2021-02-03 18:02:04 +0800

    [Update] 更新配置以支持离线访问

--- .umirc.ts
+++ .umirc.ts
@@ -42,7 +42,7 @@ export default defineConfig({
         skipWaiting: true,
         clientsClaim: true,
         navigateFallback: '/index.html',
-        globIgnores: ['robot.txt'],
+        exclude: ['robot.txt'],
         runtimeCaching: [
           {
             urlPattern: /(.*\.)?umi\.(.+)\.(js|css)/, //umi文件,带hash

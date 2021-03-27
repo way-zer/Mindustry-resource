57a566f9cb522608178455cb692165c3b4aed61e
way-zer <himc.wicp@gmail.com>
2021-03-27 21:13:38 +0800

    [Update](\game) 增加itch下载提示

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -16,44 +16,33 @@ export default function() {
     return 'https://gh.api.99988866.xyz/' + raw;
   }
 
-  function subCard(title: string, list: Release[]) {
-    return (
-      <Card title={title}>
-        <Collapse
-          accordion
-          defaultActiveKey={1}
-          bordered={false}
-          expandIconPosition={'right'}
-        >
-          {list.map(release => (
-            <Collapse.Panel
-              key={release.tag_name}
-              header={
-                <a href={release.html_url} target="_blank">
-                  {release.tag_name}
-                </a>
-              }
-            >
-              <List>
-                {release.assets.map(asset => (
-                  <List.Item
-                    key={asset.name}
-                    actions={[
-                      <a href={getDownloadURL(asset.browser_download_url)}>
-                        下载
-                      </a>,
-                    ]}
-                  >
-                    <strong>{asset.name}</strong>
-                    <small>{(asset.size / 1024 / 1024).toFixed(2)} MB</small>
-                  </List.Item>
-                ))}
-              </List>
-            </Collapse.Panel>
+  function ReleaseList(prop: { list: Release[] }) {
+    const item = (release: Release) => (
+      <Collapse.Panel
+        key={release.tag_name}
+        header={
+          <a href={release.html_url} target="_blank">
+            {release.tag_name}
+          </a>
+        }
+      >
+        <List>
+          {release.assets.map(asset => (
+            <List.Item key={asset.name} actions={[<a href={getDownloadURL(asset.browser_download_url)}>下载</a>]}>
+              <strong>{asset.name}</strong>
+              <small>{(asset.size / 1024 / 1024).toFixed(2)} MB</small>
+            </List.Item>
           ))}
+        </List>
+      </Collapse.Panel>
+    );
+    return (
+      <>
+        <Collapse accordion defaultActiveKey={1} bordered={false} expandIconPosition={'right'}>
+          {prop.list.map(it => item(it))}
         </Collapse>
-        {list.length == 0 ? <Empty /> : ''}
-      </Card>
+        {prop.list.length == 0 && <Empty />}
+      </>
     );
   }
 
@@ -71,8 +60,25 @@ export default function() {
         />,
       ]}
     >
-      {subCard('正式版', releases)}
-      {subCard('6.0 BE 测试版', beReleases)}
+      <Card title={'正式版'}>
+        <ReleaseList list={releases} />
+        <details>
+          <summary>
+            apk等版本请前往
+            <a href={'https://anuke.itch.io/mindustry'} target="_blank">
+              官方itch下载站
+            </a>
+          </summary>
+          点击Download Now,然后在弹出的对话框内点击No,Thanks即可免费下载
+          <br />
+          IOS版本仅可以在AppStore付费功能
+          <br />
+          <b>支持作者,可以选择付费,或者在Steam购买正版</b>
+        </details>
+      </Card>
+      <Card title={'6.0 BE 测试版'}>
+        <ReleaseList list={beReleases} />
+      </Card>
       <p>所有下载均来自github.com</p>
     </PageHeader>
   );
@@ -86,7 +92,5 @@ interface Release {
 }
 
 function getRelease(repo: string, perPage: number): Promise<Release[]> {
-  return request(
-    'https://api.github.com/repos/' + repo + '/releases?per_page=' + perPage,
-  );
+  return request('https://api.github.com/repos/' + repo + '/releases?per_page=' + perPage);
 }

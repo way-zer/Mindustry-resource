77fa940032b8b302ef6934bbcb07f42d30520f87
way-zer <himc.wicp@gmail.com>
2022-09-25 23:44:17 +0800

    :sparkles: 更新关于页

--- src/views/about.vue
+++ src/views/about.vue
@@ -6,18 +6,20 @@ meta:
 <template>
   <div style="padding: 0 10%">
     <h2>本资源站由WayZer搭建并运营</h2>
-    <p>内容均由网友分享添加,与本人无关.如有问题可以邮箱联系 me@wayzer.top</p>
+    <strong>欢迎会有兴趣且vue3开发的加入</strong>
+    <p>内容均由网友分享添加,与本人无关.如有问题可以邮箱反馈联系</p>
+    <p>联系邮箱: me@wayzer.top</p>
     <details>
       <summary>
-        资源站目前完全公益制作,如果觉得不错,可以考虑<a href='https://afdian.net/@WayZer'>赞助</a>
+        资源站目前完全公益制作,如果觉得不错,可以考虑<a href='https://afdian.net/@WayZer' rel="nofollow">赞助</a>
       </summary>
-      <img src='/src/assets/wechat.png' alt='微信二维码'/>
+      <img src='../assets/赞助.png' alt='微信二维码'/>
     </details>
-    <h2>其他资源</h2>
+    <h2>其他项目</h2>
     <ul>
-      <li>
-        <a href='https://git.io/SA4Mindustry'>服务器基础管理插件</a>
-      </li>
+      <li><a href='https://git.io/SA4Mindustry' rel="nofollow">服务器插件 - ScriptAgent4Mindustry</a></li>
+      <li><a href='https://github.com/TinyLake/MindustryX' rel="nofollow">增强第三方服务端 - MindustryX</a></li>
+      <li><a href='https://github.com/way-zer/ContentsTweaker' rel="nofollow">属性修改器Mod - ContentsTweaker</a></li>
     </ul>
   </div>
 </template>

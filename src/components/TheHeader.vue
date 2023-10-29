afd865b986ce1026367a205471550eea4842494f
WayZer <user@wayzer.cf>
2023-10-29 10:32:37 +0800

    fix-header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <el-menu router mode="horizontal" :default-active="navRoutes.find(it=>$route.path.startsWith(it.path))?.path">
-    <li class="inline-flex flex-none items-center w-auto">
+    <el-menu-item disabled>
       <img src="/src/assets/icons-64.png" alt="logo" class="h-12 hidden md:block">
       <span class="text-22px font-bold" style="color: dodgerblue">Mindustry 资源站</span>
-    </li>
+    </el-menu-item>
     <el-menu-item v-for="route in navRoutes" :key="route.path" :index="route.path" :route="route.path">
       {{ route.name }}
     </el-menu-item>

3800724e78504ff4ddb36fbbc5006f43c7c5092c
WayZer <user@wayzer.cf>
2023-11-13 00:08:30 +0800

    clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,14 @@ export default defineNuxtConfig({
       ],
       script: [
         { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4298329918186847", async: true, crossorigin: "anonymous" },
-        { src: "https://www.clarity.ms/tag/gsj4e9xc0u", async: true },
+        { type: "text/javascript", textContent: `
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gsj4e9xc0u");
+</script>`},
       ]
     }
   },

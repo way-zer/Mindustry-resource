d550a65594160f8c0a2e98132e13dbacddf2d4ee
WayZer <user@wayzer.cf>
2023-12-02 16:22:53 +0800

    更新依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,18 +37,16 @@ export default defineNuxtConfig({
       script: [
         { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4298329918186847", async: true, crossorigin: "anonymous" },
         {
-          type: "text/javascript", textContent: `
-        <script type="text/javascript">
+          type: "text/javascript", innerHTML: `
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "gsj4e9xc0u");
-</script>`},
+    })(window, document, "clarity", "script", "gsj4e9xc0u");`},
       ]
     }
   },
-  nitro: {
+nitro: {
     preset: "vercel-edge",
   },
   vite: {

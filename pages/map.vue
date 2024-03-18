42f8256e9014f6143a5e62c87be31d8964704c82
WayZer <user@wayzer.cf>
2024-03-18 22:12:09 +0800

    update dependencies

--- pages/map.vue
+++ pages/map.vue
@@ -8,29 +8,29 @@
     <div class="filter">
       <b>按模式筛选: </b>
       <el-radio-group size="small" :model-value="getTag('mode')" @change="(v) => { replaceTag('mode', v) }">
-        <el-radio-button v-for="mode in gameModes" :key="mode" :label="mode" />
-        <el-radio-button label="X" />
+        <el-radio-button v-for="mode in gameModes" :key="mode" :value="mode">{{ mode }}</el-radio-button>
+        <el-radio-button value="X" />
       </el-radio-group>
     </div>
     <div class="filter">
       <b>按游戏版本筛选: </b>
       <el-radio-group size="small" :model-value="getTag('version')" @change="(v) => { replaceTag('version', v) }">
-        <el-radio-button label="3">v5(104)</el-radio-button>
-        <el-radio-button label="4">v6(126)</el-radio-button>
-        <el-radio-button label="5">v7(135)</el-radio-button>
-        <el-radio-button label="7">v7.5(136/be)</el-radio-button>
-        <el-radio-button label="X" />
+        <el-radio-button value="3">v5(104)</el-radio-button>
+        <el-radio-button value="4">v6(126)</el-radio-button>
+        <el-radio-button value="5">v7(135)</el-radio-button>
+        <el-radio-button value="7">v7.5(136/be)</el-radio-button>
+        <el-radio-button value="X" />
       </el-radio-group>
     </div>
     <div class="filter">
       <b>排序方式: </b>
       <el-radio-group size="small" :model-value="getTag('sort') || 'X'" @change="(v) => { replaceTag('sort', v) }">
-        <el-radio-button label="X">热度</el-radio-button>
-        <el-radio-button label="updateTime">更新时间</el-radio-button>
-        <el-radio-button label="createTime">发布时间</el-radio-button>
-        <el-radio-button label="download">下载量</el-radio-button>
-        <el-radio-button label="rating">评分</el-radio-button>
-        <el-radio-button label="like">点赞数</el-radio-button>
+        <el-radio-button value="X">热度</el-radio-button>
+        <el-radio-button value="updateTime">更新时间</el-radio-button>
+        <el-radio-button value="createTime">发布时间</el-radio-button>
+        <el-radio-button value="download">下载量</el-radio-button>
+        <el-radio-button value="rating">评分</el-radio-button>
+        <el-radio-button value="like">点赞数</el-radio-button>
       </el-radio-group>
     </div>
     <MapList />

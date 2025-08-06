fd6a67036a83bc815dbafa7070adca6f78c0ba1e
WayZer <himc.wicp@gmail.com>
2025-08-07 00:27:09 +0800

    👽️ 支持筛选150地图

--- app/pages/map/index.vue
+++ app/pages/map/index.vue
@@ -19,7 +19,8 @@
         <el-radio-button value="4">v6(126)</el-radio-button>
         <el-radio-button value="5">v7(135)</el-radio-button>
         <el-radio-button value="7">v7.5(136-146)</el-radio-button>
-        <el-radio-button value="8">v8(147-BE)</el-radio-button>
+        <el-radio-button value="8">v8a(147-149)</el-radio-button>
+        <el-radio-button value="9">v8b(150+)</el-radio-button>
         <el-radio-button :value="false">X</el-radio-button>
       </el-radio-group>
     </div>

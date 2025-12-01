a82e3ab2e17cf0accb21fb81de336fe7969f5a13
WayZer <himc.wicp@gmail.com>
2025-12-01 21:52:51 +0800

    useClipboard

--- app/pages/map/components/ActionCopy.vue
+++ app/pages/map/components/ActionCopy.vue
@@ -1,11 +1,11 @@
 <template>
-  <el-button ref="buttonRef" @click="copy" :link="!props.circle" :circle="props.circle">
-    <Icon name="ep:document-copy"/>
+  <el-button ref="buttonRef" @click="copy()" :link="!props.circle" :circle="props.circle">
+    <Icon name="ep:document-copy" />
     <client-only>
-      <el-tooltip content="拷贝换图指令" virtual-triggering :virtual-ref="buttonRef"/>
+      <el-tooltip content="拷贝换图指令" virtual-triggering :virtual-ref="buttonRef" />
       <el-popover trigger="click" :persistent="false" width="unset" virtual-triggering :virtual-ref="buttonRef">
         粘贴指令到支持网络换图的服务器使用
-        <pre ref="commandRef">/vote map {{ thread }}</pre>
+        <pre ref="commandRef">{{ content }}</pre>
       </el-popover>
     </client-only>
   </el-button>
@@ -22,9 +22,15 @@ const props = defineProps({
 })
 
 const buttonRef = ref<HTMLElement>()
-const commandRef = ref<Node>()
-
-function copy() {
-  copyContent(() => (commandRef.value!!))
-}
+const commandRef = useTemplateRef<Node>("commandRef")
+const content = computed(() => `/vote map ${props.thread}`)
+const { copy, copied } = useClipboard({
+  source: content,
+  legacy: true,
+})
+watchPostEffect(() => {
+  if (copied.value) {
+    ElMessage.success('换图指令已复制到剪贴板')
+  }
+})
 </script>
